9e8f5e804056867aeb50f245666e83672e813f26
MAMOSKO <69401772+MAMOSKO@users.noreply.github.com>
2022-04-10 02:50:58 +0300

    Update passwordInput.py

--- passwordInput.py
+++ passwordInput.py
@@ -1,47 +1,59 @@
+# -*- coding: utf-8 -*-
 from pynput.keyboard import Key, Listener
+from threading import Thread
+
 
 characters="abcçdefgğhıijklmnoçprsştuüvyzwxq1234567890.,_-!+%"
 
-
 password=""
+passwordText=""
 
-class passwordInput():
-    def __init__(self,passtext=""):
-        self.passtext=passtext
-        print(str(self.passtext),end="\r")
-        self.password=""
-        def on_press(key):
-            if key==Key.backspace and self.password != "":
-                l=len(self.password)
-                self.password=self.password[:l-1]
-            elif key==Key.enter:
-                print("")
-                global password
-                password=self.password
-                return False
-            else:
-                if str(key).replace("'","") in characters or key==Key.space:
-                    if key==Key.space:
-                        self.password=self.password+" "
-                    else:
-                        self.password=self.password+str(key).replace("'","")
-            print(self.changeText2(self.password),end="\r")
-            print(str(self.passtext)+self.changeText(self.password),end="\r")
-        with Listener(on_press=on_press) as listener:
-            listener.join()
-            if listener=="":
-                return ""
-    def changeText(self,text):
-        i=0
-        returnText=""
-        while i<len(text):
-            returnText=returnText+"*"
-            i+=1
-        return returnText
-    def changeText2(self,text):
-        i=-20
-        returnText=""
-        while i<len(text):
-            returnText=returnText+" "
-            i+=1
-        return returnText
+def changeTexta(text,i,a):
+    "FUNCTION FOR passwordInput"
+    returnText=""
+    while i<len(text):
+        returnText=returnText+a
+        i+=1
+    return returnText
+
+
+def passwordInput(passtext=""):
+    global password
+    print(str(passtext),end="\r")
+    def on_press(key):
+        global password
+        if key==Key.backspace and password != "":
+            l=len(password)
+            password=password[:l-1]
+        elif key==Key.enter:
+            print("")
+            global passwordText
+            passwordText=password
+            return False
+        else:
+            if str(key).replace("'","") in characters or key==Key.space:
+                if key==Key.space:
+                    password=password+" "
+                else:
+                    password=password+str(key).replace("'","")
+        print(changeTexta(password,-20," "),end="\r")
+        print(str(passtext)+changeTexta(password,0,"*"),end="\r")
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+        if listener=="":
+            return ""
+    return passwordText
+
+
+passwordInput.__doc__="""
+Kullanimi "input" fonksiyonu ile aynidir.
+Its usage is the same as the "input" function.
+
+Ornek/Example:
+from passwordInput import passwordInput
+password=passwordInput.passwordInput("Enter Password: ")
+print("Password is "+password)
+
+https://mamosko.ml
+https://github/MAMOSKO
+"""
